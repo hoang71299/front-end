9a6103758c5ad476528027df39aa9bd1adc5258a
huynh hoang <huynhhoang712003@gmail.com>
2023-12-25 00:39:09 +0700

    module

---
+++ es6/list.js
@@ -0,0 +1,11 @@
+function sum(n1, n2) {
+    console.log(n1 + n2);
+}
+function times(n1, n2) {
+    console.log(n1 + n2);
+}
+function minus(n1, n2) {
+    console.log(n1 - n2);
+}
+export { times, minus } 
+export default sum
---
+++ es6/module.js
@@ -0,0 +1,4 @@
+import sum, * as MyMath from './list.js'
+console.log(sum(10, 20));
+console.log(MyMath.minus(30, 20));
+console.log(MyMath.times(30, 20));
