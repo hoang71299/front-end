3504c552bdcc9aa41a932392ccbb5e53aac1f5f3
huynh hoang <huynhhoang712003@gmail.com>
2023-12-24 23:31:26 +0700

    arrow function

---
+++ es6/arrow-function.js
@@ -0,0 +1,13 @@
+//arrow function
+const times = (n1, n2) => console.log(n1 * n2);
+times(10, 20)
+const minus = (n1, n2) => n1 - n2;
+
+console.log(minus(10, 20));
+
+const alert = (message) => `alert ${message}`
+
+// console.log(alert("chuẩn bị thi"));
+const slogun = () => `still breath still alive`
+
+console.log(slogun());
